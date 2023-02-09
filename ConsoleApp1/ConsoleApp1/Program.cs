1851ccfbbb8db1fa1eb1739eb211eb676dd1395a
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-02-09 22:43:44 +0200

    sa

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter your kilograms: ");
+            int kg = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            double BMI = kg / Math.Pow(height, 2);
+
+            if (BMI < 18.5)
+            {
+                Console.WriteLine("Low");
+            }
+            else if (BMI >= 18.5 && BMI < 25)
+            {
+                Console.WriteLine("Normal");
+            }
+            else
+            {
+                Console.WriteLine("High");
+            }
         }
     }
 }
